651b5e32463131b09738532b37c9f2d7d5618252
Jorge Arturo Rodriguez Martin <joarceus@hotmail.com>
2021-08-01 15:03:33 -0500

    Separation of files and more for the API

--- models/server.js
+++ models/server.js
@@ -1,5 +1,7 @@
 
 const express = require('express');
+const cors = require('cors');
+
 require('dotenv').config();
 
 
@@ -9,22 +11,26 @@ class Server {
     constructor() {
         this.app = express();
         this.port = process.env.PORT;
+        this.usersRoutePath = '/api/users';
 
         this.middlewares(); // order matters
         this.routes(); // triggers the init of the routes when called
     }
 
     middlewares() {
+        //CORS
+        this.app.use(cors());
+
+        //Parse and lecture of body
+        this.app.use(express.json());
+
         // we connect our public resources to static express content
         this.app.use(express.static('public'));
     }
 
     routes() {
 
-        this.app.get('/api', (req, res) => {
-            res.send('Hello world')
-        });
-
+        this.app.use(this.usersRoutePath, require('../routes/user'));
     }
 
     listen() {
